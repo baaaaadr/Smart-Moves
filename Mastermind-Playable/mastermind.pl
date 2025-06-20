d8589298cde78fc7f05ada092b269b2128a726bd
Badr Akoum <bdrakoum@gmail.com>
2025-06-20 19:54:44 +0200

    cleanup

--- Mastermind-Playable/mastermind.pl
+++ Mastermind-Playable/mastermind.pl
@@ -1,4 +1,4 @@
-%question 1.1
+%Renvoie le nombre de numéro bien placés
 nBienPlace([], [], 0).
 nBienPlace([T|Code1], [T|Code2], BP) :- 
     nBienPlace(Code1, Code2, BPRestant),
@@ -7,20 +7,22 @@ nBienPlace([T1|Code1], [T2|Code2], BP) :-
     T1 \= T2,
     nBienPlace(Code1, Code2, BP).
 
+%==============================
 
-%==============================question 1.2
-    %pour une liste L donne sa longueur N
+%Pour une liste L donne sa longueur N
 longueur([], 0).
 longueur([_|L], N):-
     longueur(L,NRestant),
     N is NRestant+1.
+
+%Vrai si les codes sont equivalents
 gagne(Code1, Code2) :- 
     longueur(Code1,N),
     longueur(Code2,N),
     nBienPlace(Code1,Code2,N).
 
+%==============================
 
-%==============================question 2
     %element E is in L
 %element([], _).
 element(E, [E|_]).
@@ -35,6 +37,8 @@ enleve(E, [H|L1], [H|L2]):- %on ajoute le H devant L2 la7atta ylaz2o fi bl outpu
     enleve(E,L1,L2).
 
 %PS: interdt de mettre predicat = predicat, c est pas des fonctions c des booleen ett
+
+%Enleve les bien placés
 enleveBP([],[], [],[]).
 enleveBP([T|Code1], [T|Code2], Code1Bis, Code2Bis):-
     enleveBP(Code1, Code2, Code1Bis, Code2Bis).
@@ -42,9 +46,9 @@ enleveBP([T1|Code1], [T2|Code2], [T1|Code1Bis], [T2|Code2Bis]):-
     T1 \= T2,
     enleveBP(Code1,Code2, Code1Bis, Code2Bis).
 
+%==============================
+%!!!important
 
-%==============================question2.4
-%!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 nMalPlacesAux([], _, 0). %!!!!! underscore not [], sinon it would only work when tous les elements sont communs dans les deux listes
 nMalPlacesAux([T|Code1], Code2, MP):-
     element(T, Code2),
@@ -55,20 +59,22 @@ nMalPlacesAux([T|Code1], Code2, MP):-
 nMalPlacesAux([T|Code1], Code2, MP):-
     \+ element(T, Code2),
     nMalPlacesAux(Code1, Code2, MP).
-%basically, addesh fi mal placé? yaane kam number mawjoud b une ou des posiitons qui sont fausses
 
+%basically, cb ya de mal placé? yaane kam number mawjoud b une ou des posiitons qui sont fausses
 nMalPlaces(Code1, Code2, MP):-
     enleveBP(Code1, Code2, Code1Bis, Code2Bis),
     nMalPlacesAux(Code1Bis, Code2Bis, MP).
 
-%==============================question3
-%produit random code de taille N, et de M couleurs
+%==============================
+
+%Produit random code de taille N, et de M couleurs
+%====V1
 codeur(M,N,Code):-
     codeur_aux(M,N,[],Code). % on utilise un accumulateur, appel initial avec Acc=liste vide
 
 codeur_aux(_,0,Acc,Code):-	 % Quand N = 0, on retourne la liste construite en unifiant Code avec Acc
 	Code=Acc.
-
+ 
 codeur_aux(M, N, Acc, Code):-
     %N>0,
     M1 is M+1,
@@ -76,6 +82,7 @@ codeur_aux(M, N, Acc, Code):-
     N1 is N-1,
     codeur_aux(M, N1, [Number|Acc], Code). % On ajoute Number à Acc
 
+%====V2
 codeur2(_,0,[]).
 codeur2(NbCouleurs, Taille, [Number|Code]):-
     NbCouleurs2 is NbCouleurs+1,
@@ -83,6 +90,7 @@ codeur2(NbCouleurs, Taille, [Number|Code]):-
     Taille2 is Taille-1,
     codeur2(NbCouleurs, Taille2, Code).
 
+%====V3
 codeur3(_, 0, []) :- !.
 codeur3(NbCouleurs, Taille, [New|Code]) :-
     Max is NbCouleurs + 1,
@@ -90,7 +98,8 @@ codeur3(NbCouleurs, Taille, [New|Code]) :-
     Taille2 is Taille - 1,
     codeur(NbCouleurs, Taille2, Code).
 
-%==============================question4
+%==============================
+
 jouons(NbCouleurs, Taille, Max) :-
     codeur(NbCouleurs, Taille, CodeATrouver),
     play(NbCouleurs, Max, [], [], CodeATrouver).
